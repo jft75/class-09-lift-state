476553c43d6d9c95e18168147428a886316bb5b0
jft75 <143815415+jft75@users.noreply.github.com>
2025-02-24 16:25:52 +0000

    complete class9

--- src/components/AccordionPanel.jsx
+++ src/components/AccordionPanel.jsx
@@ -1,15 +1,17 @@
 // source: https://react.dev/learn/sharing-state-between-components
-import { useState } from 'react';
 
 export default function AccordionPanel({
   title,
-  children
+  children,
+  isExpanded,
+  onExpand
 }) {
-  const [isExpanded, setIsExpanded] = useState(false);
-
   return (
     <section className="panel">
-      <button onClick={() => setIsExpanded(!isExpanded)}>
+      <button
+        // onClick={() => onExpand()}
+        onClick={onExpand}
+      >
         <h3>{title}</h3>
       </button>
       {isExpanded &&
